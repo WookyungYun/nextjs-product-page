db457bdac5015c89ab7ee16e42bd8219123f2884
WookyungYun <gyong1540@gmail.com>
2025-04-19 22:25:33 +0900

    Feat: 상품 개별조회 api 추가

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from '../constants/apiConstants';
-import { FetchProductsParams, ProductsResponse } from '../types/product';
+import { FetchProductsParams, Product, ProductsResponse } from '../types/product';
 
 // 상풍 리스트
 export const fetchProducts = async ({ skip = 0, query = '' }: FetchProductsParams): Promise<ProductsResponse> => {
@@ -16,5 +16,12 @@ export const fetchProducts = async ({ skip = 0, query = '' }: FetchProductsParam
     throw new Error('api error');
   }
   const data: ProductsResponse = await response.json();
+  return data;
+};
+
+// 상풍 개별조회
+export const fetchProduct = async (id: string): Promise<Product> => {
+  const response = await fetch(`${BASE_URL}/${id}`);
+  const data: Product = await response.json();
   return data;
 };
