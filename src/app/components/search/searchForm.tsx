5db2fc02882233ddf1bbe7d322e01300636f0d58
WookyungYun <gyong1540@gmail.com>
2025-04-20 00:09:00 +0900

    Fix: submit 함수 추가

--- src/app/components/search/searchForm.tsx
+++ src/app/components/search/searchForm.tsx
@@ -1,12 +1,32 @@
-import { SearchFormProps } from '@/app/types/product';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+import { Form, FormContainer, SearchButton, SearchRow, StyledInput } from './search.style';
+
+interface SearchFormProps {
+  defaultQuery: string;
+}
 
 export default function SearchForm({ defaultQuery }: SearchFormProps) {
+  const [query, setQuery] = useState(defaultQuery);
+  const router = useRouter();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const trimmed = query.trim();
+    router.push(`/?query=${encodeURIComponent(trimmed)}`);
+  };
+
   return (
-    <>
-      <form>
-        <input type='text' name='query' defaultValue={defaultQuery} placeholder='상품명을 검색해보세요' />
-        <button type='submit'>검색</button>
-      </form>
-    </>
+    <Form onSubmit={handleSubmit}>
+      <FormContainer>
+        <SearchRow>
+          <StyledInput type='text' name='query' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='상품명을 검색해보세요' />
+          <SearchButton type='submit'>검색</SearchButton>
+        </SearchRow>
+      </FormContainer>
+    </Form>
   );
 }
